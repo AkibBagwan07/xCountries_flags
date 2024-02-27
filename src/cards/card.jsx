29ed0065eed01c8e6205352bf201a7b993165602
Akib Bagwan <akibbagwan69@gmail.com>
2024-02-27 20:38:11 +0530

    first commit

---
+++ src/cards/card.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./card.module.css";
+
+let Api = "https://restcountries.com/v3.1/all";
+
+export default function Card() {
+  let [countries, setCountries] = useState([]);
+  useEffect(() => {
+    try {
+      async function getdata() {
+        let data = await axios.get(Api);
+        setCountries(data.data);
+        console.log(data.data);
+        return data;
+      }
+      getdata();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  return (
+    <div className={styles.parent}>
+      {countries.map((country) => (
+        <div className={styles.card} key={country.cca3}>
+          <img
+            className={styles.flag}
+            src={country.flags.png}
+            alt={country.name.common}
+          />
+          <h5 className={styles.name}>{country.name.common}</h5>
+        </div>
+      ))}
+    </div>
+  );
+}
